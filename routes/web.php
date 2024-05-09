8ed8f8a3159018dbe13c09d4709d3a95c8a1f803
Wildan Malul Azam <malula0101@gmail.com>
2024-05-09 19:05:45 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', function () {
+Route::get('/p', function () {
     return 'cek';
 });
