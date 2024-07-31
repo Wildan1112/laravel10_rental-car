0b52f90f578fa720bccb02a1f339d783a5d572ab
Wildan Malul Azam <malula0101@gmail.com>
2024-07-31 16:49:33 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/wka', function () {
-    return 'cek';
+
+Route::get('/hello-world', function () {
+    return 'Hello World!';
 });
